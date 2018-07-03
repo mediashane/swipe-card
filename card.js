450142cda6e1c014af6be206742c98afad686859
Shane <shane@mediashane.com>
2018-07-03 13:16:43 -0700

    Update card.js

--- card.js
+++ card.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   View,
   Image,
   Text,
@@ -13,10 +12,11 @@ import { Icon } from 'react-native-elements';
 import * as firebase from 'firebase';
 import base64 from 'base-64';
 
+// Gets Dimensions of User Screen
 const { width, height } = Dimensions.get('window');
 
-const styles = StyleSheet.create({
-  card: {
+const styles = {
+  swipeCard: {
     position: 'absolute',
     width: width - 20,
     height: height * 0.82,
@@ -27,11 +27,28 @@ const styles = StyleSheet.create({
     borderColor: 'lightgrey',
     borderRadius: 8,
   },
-});
+  itemDescription: { 
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    padding: 15,
+    borderRadius: 8,
+    backgroundColor: 'rgba( 255, 255, 255, .9)',
+  },
+  buttonGroup: {
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    bottom: 5,
+  },
+};
 
 export default class Card extends Component {
+
   componentWillMount() {
     this.pan = new Animated.ValueXY();
+    const { listingId } = this.props.profile;
 
     // PanResponder Object
     this.cardPanResponder = PanResponder.create({
@@ -43,24 +60,24 @@ export default class Card extends Component {
         // Updates Animated Values
         { dx: this.pan.x, dy: this.pan.y },
       ]),
-      // Determines how much Card has moved and which direction
+      // Determines how much Card has moved and which swipeDirection
       onPanResponderRelease: (e, { dx }) => {
         // Gets absolute value of movement
         const absDx = Math.abs(dx);
-        // Determines swipe direction
-        const direction = absDx / dx;
-        // Statement returns true if card is swiped to the positive direction, i.e. to the right
-        const swipedRight = direction > 0;
+        // Determines swipe swipeDirection
+        const swipeDirection = absDx / dx;
+        // swipedRight returns true if card is swiped to the positive swipeDirection, i.e. to the right
+        const swipedRight = swipeDirection > 0;
 
         // Threshold to Swipe Card off Screen
         if (absDx > 120) {
-          // Animates Card off Screen
+          // Animates Card off Screen if threshold is exceeded
           Animated.decay(this.pan, {
-            velocity: { x: 3 * direction, y: 0 },
+            velocity: { x: 3 * swipeDirection, y: 0 },
             deceleration: 0.995,
-          }).start(() => this.props.onSwipeOff(swipedRight, this.props.profile.listingId));
+          }).start(() => this.props.onSwipeOff(swipedRight, listingId));
         } else {
-          // Returns Card to Center when Dropped
+          // Returns card to center if dropped before reaching swipe off screen threshold
           Animated.spring(this.pan, {
             toValue: { x: 0, y: 0 },
             friction: 4.5,
@@ -70,9 +87,37 @@ export default class Card extends Component {
     });
   }
 
+  flagAsInappropriate = () => {
+    const { title, uid, listingId } = this.props.profile;
+    Alert.alert(
+      'Flag as Inappropriate?',
+      `Are you SURE you'd like to report the ${title} as inappropriate?`,
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancelled') },
+        { text: 'Yes',
+          onPress: () => {
+            Animated.spring(this.pan, {
+              toValue: { x: -600, y: 0 },
+              friction: 3.5,
+            }).start(() => this.props.onSwipeOff(false, listingId));
+            firebase.database().ref(`listingsBySeller/${uid}/${listingId}/flags`)
+            .transaction((currentFlags) => {
+              return currentFlags + 1;
+            });
+            firebase.database().ref(`users/${uid}/flags`)
+            .transaction((currentFlags) => {
+              return currentFlags + 1;
+            });
+          } },
+      ],
+      { cancelable: true },
+    );
+  }
+
   render() {
-    // Expands profile prop into individual variables values
-    const { title, price, description, imageByteArray, uid, first_name } = this.props.profile;
+    // Destructures profile props into individual variables values
+    const { title, price, description, imageByteArray } = this.props.profile;
+    // Decodes base64 image received from Firebase database);
     let decodedImageByteArray = base64.decode(imageByteArray);
     // Interpolates touch gesture values to degrees
     const rotateCard = this.pan.x.interpolate({
@@ -101,80 +146,64 @@ export default class Card extends Component {
       <Animated.View
         // Spread operator deconstructs panHandlers Object
         {...this.cardPanResponder.panHandlers}
-        style={[styles.card, animatedStyle, overlayStyle]}
+        style={[ styles.swipeCard, animatedStyle, overlayStyle ]}
       >
         <Image
           style={{ flex: 1, borderRadius: 7 }}
           source={{ uri: `data:image/jpeg;base64,${decodedImageByteArray}` }}
-          borderRadius={7}
+          borderRadius={ 7 }
         >
-          <Animated.View style={[overlayStyle]} />
+        
+        <Animated.View style={[ overlayStyle ]} />
 
-        <View style={{ marginBottom: 10, marginTop: 10, marginLeft: 15, marginRight: 15, padding: 15, borderRadius: 8, backgroundColor: 'rgba( 255, 255, 255, .9)' }}>
+        <View style={ styles.itemDescription}>
           <Text style={{ fontSize: 20 }}>{ title }, ${ price }</Text>
           {// if description variable is assigned it's displayed here, if not an empty View is
             description ? <Text style={{ fontSize: 15, color: '#393939' }}>{ description }</Text> : <View />}
         </View>
-        <View style={{ alignItems: 'flex-end', justifyContent: 'space-around', flexDirection: 'row', bottom: 5 }}>
+
+        <View style={ styles.buttonGroup }>
+
           <Icon
             raised
             reverse
             name='times'
             type='font-awesome'
             color='rgba( 128, 128, 128, .9)'
-            size={30}
+            size={ 30 }
             onPress={() => Animated.spring(this.pan, {
               toValue: { x: -600, y: 0 },
               friction: 3.5,
-            }).start(() => this.props.onSwipeOff(false, this.props.profile.listingId))}
+            }).start(() => this.props.onSwipeOff(false, listingId))}
           />
+
           <Icon
             raised
             reverse
             name='flag'
             type='font-awesome'
             color='rgba( 193, 85, 85, .9)'
-            size={15}
-            onPress={() =>
-              Alert.alert(
-                'Flag as Inappropriate?',
-                `Are you SURE you'd like to report the ${this.props.profile.title} as inappropriate?`,
-                [
-                  { text: 'Cancel', onPress: () => console.log('Cancelled') },
-                  { text: 'Yes',
-                    onPress: () => {
-                      Animated.spring(this.pan, {
-                        toValue: { x: -600, y: 0 },
-                        friction: 3.5,
-                      }).start(() => this.props.onSwipeOff(false, this.props.profile.listingId));
-                      firebase.database().ref(`listingsBySeller/${this.props.profile.uid}/${this.props.profile.listingId}/flags`)
-                      .transaction((currentFlags) => {
-                        return currentFlags + 1;
-                      });
-                      firebase.database().ref(`users/${this.props.profile.uid}/flags`)
-                      .transaction((currentFlags) => {
-                        return currentFlags + 1;
-                      });
-                    } },
-                ],
-                { cancelable: true },
-              )
-              }
+            size={ 15 }
+            onPress={() => this.flagAsInappropriate()}
           />
-            <Icon
-              raised
-              reverse
-              name='heart'
-              type='font-awesome'
-              color='rgba( 0, 127, 123, .9)'
-              size={30}
-              onPress={() => Animated.spring(this.pan, {
-                toValue: { x: 600, y: 0 },
-                friction: 3.5,
-              }).start(() => this.props.onSwipeOff(true, this.props.profile.listingId))}
-            />
+
+          <Icon
+            raised
+            reverse
+            name='heart'
+            type='font-awesome'
+            color='rgba( 0, 127, 123, .9)'
+            size={ 30 }
+            onPress={() => Animated.spring(this.pan, {
+              toValue: { x: 600, y: 0 },
+              friction: 3.5,
+            }).start(() => this.props.onSwipeOff(true, listingId))}
+          />
+
         </View>
+
         </Image>
+
       </Animated.View>
     );
   }
